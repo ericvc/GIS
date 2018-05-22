c98e5090184253759373d0aee405e05e0364988e
ericvc <evancleave@ucdavis.edu>
2018-05-22 11:24:06 -0700

    error in script
    index error prevented results from being saved -_-. other modifications to code to (slightly) improve efficiency.

--- weightedHabUse.R
+++ weightedHabUse.R
@@ -3,7 +3,7 @@ library(move)
 library(rgdal)
 library(raster)
 
-weightedHabUse = function (x, range.subset, ts, ras, le, hab) 
+weightedHabUse = function (x, range.subset, ts, ras, le, h) 
 {
   object <- x@DBMvar
   outMatrix = matrix(NA, length(range.subset), ncol=11)
@@ -16,32 +16,25 @@ weightedHabUse = function (x, range.subset, ts, ras, le, hab)
   for (i in range.subset){
     object@interest <- rep(FALSE, nrow(object))
     object@interest[i] <- TRUE
-    times = object@data$time[i]
-    var = object@means[i]
-    extent.compare = cbind(extent(object)[],extent(object[i+1])[])
-    n = 1e2
-    extent. = c(xmin=min(extent.compare[1,])-n, xmax=max(extent.compare[2,])+n, 
-                ymin=min(extent.compare[3,])-n, ymax=max(extent.compare[4,])+n)
-    x.out <- brownian.bridge.dyn(object, raster = ras, time.step = ts, bbox = extent.,
+    x.out <- brownian.bridge.dyn(object, raster = ras, time.step = ts,
                                  location.error = le)
-    hab.crop = projectRaster(hab, x.out, method="ngb")
-    outMatrix[i-(min(range.subset)-1),] = tapply(x.out[], list(hab.crop[]), sum)
+    outMatrix[i-(min(range.subset)+1),] = tapply(x.out[], list(crop(h, x.out, method="ngb")[]), sum)
+    
   }
   outMatrix
 }
 
-leop_id = c("Chumvi","Chumvi2","Ewaso","Haraka","Konda","Limofu","Mzee","Tatu")
+leop_id = c("Chumvi","Chumvi","Ewaso","Haraka","Konda","Limofu","Mzee","Tatu")
 #probability distributions
-dbbmm = list.files("Workspace/", pattern="dbbmm", full.names=TRUE)[-7]
+dbbmm = list.files("Chapter 1/data/", pattern="dbbmm", full.names=TRUE)[-7]
 #GIS layer
-hab = raster("Environmental Variables/Habitat Categorical/Habitat_Classification_Cover_FULL_v3.tif")
+hab = raster("Environmental Variables/Habitat Categorical/Habitat_Classification_Cover_FULL_v3_STEP.tif")
 
 use = list()
 for(leopard in 1:length(leop_id)){
   d = get(load(dbbmm[leopard]))
   range.subset = which(d@DBMvar@interest==TRUE)
-  time.step = 0.25
-  use[[leopard]] = weightedHabUse(d, range.subset, ts=time.step, ras=5, hab=hab, le=12)
+  use[[leopard]] = weightedHabUse(d, range.subset, ts=0.25, ras=5, h=hab, le=12)
 }
 names(use) = leop_id
 
@@ -49,4 +42,8 @@ START <- Sys.time()
 saveRDS(use, "weightedHabUse_list.rds")
 END <- Sys.time()
 
+#print time to completion
 difftime(END, START, units = "hours")
+
+#calculations are done
+browseURL("https://www.youtube.com/watch?v=-0Ao4t_fe0I")
